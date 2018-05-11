452f2181ecc62d21e19212bfb041443ba5a60f13
Harshith Goka <harshith9399@gmail.com>
2018-05-11 14:00:29 +0530

    removed default content

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 9399
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
+app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
