d2b13ccdd0a6431a07baea30096ea2cceccd6e38
Harshith Goka <harshith9399@gmail.com>
2018-05-23 00:18:17 +0900

    new user synchronising added; added state variables on server;

--- index.js
+++ index.js
@@ -14,12 +14,27 @@ app.use(express.static(path.join(__dirname, 'public')))
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
 
-  
+
+var curr_position = 0.0;
+var position_timestamp = new Date().getTime();
+var state_timestamp = new Date().getTime();
+
+var curr_state = 'pause';
+
 io.on('connection', function (socket) {
   console.log('a user connected');
   nickname = socket.handshake.query.nickname;
   socket.broadcast.emit('user connect', nickname);
   socket.nickname = nickname;
+
+  
+  socket.emit('initstate', { state: curr_state});
+  if (curr_state == 'pause') {
+    socket.emit('position', {position: (curr_position + (state_timestamp - position_timestamp)/1000.0 )});
+  }
+  else {
+    socket.emit('position', { position: (curr_position + (new Date().getTime() - position_timestamp)/1000.0) });
+  }
 
   socket.on('disconnect', function () {
     this.broadcast.emit('user disconnect', this.nickname);
@@ -29,12 +44,16 @@ io.on('connection', function (socket) {
   socket.on('position', function(msg) {
     // this.broadcast.emit('position', msg);
     io.emit('position', msg);
+    curr_position = msg.position;
+    position_timestamp = new Date().getTime();
     console.log("position: " + msg);
   });
 
   socket.on('state', function (msg) {
     // this.broadcast.emit('state', msg);
     io.emit('state', msg);
+    curr_state = msg.state;
+    state_timestamp = new Date().getTime();
     console.log("state: " + msg);
   });
 });
